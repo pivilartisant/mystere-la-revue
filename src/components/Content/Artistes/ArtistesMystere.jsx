6e76289605da2ccb192f043136fd980eefd6502a
Pivi Lartisant <paul.lartisant@icloud.com>
2023-02-11 02:03:58 +0100

    Testing V.0.1.3

---
+++ src/components/Content/Artistes/ArtistesMystere.jsx
@@ -0,0 +1,42 @@
+export function ArtistesMystere(){
+    return (
+        <div className="mystere-artist-content">
+             <div className="mystere-artist-content-container">
+                <div className="mystere-2-artist-container">
+                    <div className="mystere-2-title-container">
+                            <p className="mystere-2-title">Numéro 2 : <br></br> La liberté en politique</p>
+                    </div>
+
+                    <div className="mystere-2-artist">
+
+                        <ul className="mystere-2-artist-list">
+                            <li>Carmen Verheijen <br></br> <em> Photographe </em> </li>
+                            <li>Emma Santos <br></br> <em> Peintre</em></li>
+                            <li>Noah Tijenda <br></br><em> Photojournaliste</em></li>
+                            <li> Mark Marra  <br></br> <em>Photojournaliste</em> </li>
+                            <li> Ali Mohamed <br></br> <em>Photojournaliste </em></li>
+                            <li>Edward Hu <br></br><em> Designer</em></li>
+
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div className="mystere-1-artist-container">
+                    <div className="mystere-1-title-container">
+                            <p className="mystere-1-title">Numéro 1 : <br></br> La Contreverse</p>
+                    </div>
+
+                    <div className="mystere-1-artist">
+                        <ul className="mystere-1-artist-list">
+                                <li>Léa Besson <br></br> <em> Graphiste</em></li>
+                                <li> Inés Vanlaeys <br></br> <em>Peintre </em></li>
+                                <li>  Arcane 17 <br></br><em> Artiste plastique</em></li>
+                                <li>  Cy <br></br><em>Artiste plastique</em></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
--- src/pages/Artistes.jsx
+++ src/pages/Artistes.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "../components/UI/Navbar";
 import { Arrow2 } from "../components/UI/Arrow2";
 import "../styles/artistes.css";
 import arrow  from "../assets/png/revue/flecheRevue.png";
+import { ArtistesMystere } from "../components/Content/Artistes/ArtistesMystere";
 
 export function Artistes() {
     return (
@@ -15,45 +16,7 @@ export function Artistes() {
 
                 <Arrow2 arrow2={arrow}/>
 
-                <div className="mystere-artist-container">
-
-                <div className="mystere-2-artist-container">
-                    <div className="mystere-2-title-container">
-                            <p className="mystere-2-title">Numéro 2 : <br></br> La liberté en politique</p>
-                    </div>
-
-                    <div className="mystere-2-artist">
-
-                        <ul className="mystere-2-artist-list">
-                            <li>Carmen Verheijen <br></br> <em> Photographe </em> </li>
-                            <li>Emma Santos <br></br> <em> Peintre</em></li>
-                            <li>Noah Tijenda <br></br><em> Photojournaliste</em></li>
-                            <li> Mark Marra  <br></br> <em>Photojournaliste</em> </li>
-                            <li> Ali Mohamed <br></br> <em>Photojournaliste </em></li>
-                            <li>Edward Hu <br></br><em> Designer</em></li>
-
-                        </ul>
-                       
-                    </div>
-
-                </div>
-
-                <div className="mystere-1-artist-container">
-                    <div className="mystere-1-title-container">
-                            <p className="mystere-1-title">Numéro 1 : <br></br> La Contreverse</p>
-                    </div>
-
-                    <div className="mystere-1-artist">
-                        <ul className="mystere-1-artist-list">
-                                <li>Léa Besson <br></br> <em> Graphiste</em></li>
-                                <li> Inés Vanlaeys <br></br> <em>Peintre </em></li>
-                                <li>  Arcane 17 <br></br><em> Artiste plastique</em></li>
-                                <li>  Cy <br></br><em>Artiste plastique</em></li>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
+               <ArtistesMystere />
                 
             </main>
 
