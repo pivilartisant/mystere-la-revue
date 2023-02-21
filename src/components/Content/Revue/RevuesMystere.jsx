932c1b3be063f9a433fe3683649b71db632bf1c3
Pivi Lartisant <paul.lartisant@icloud.com>
2023-02-22 00:18:43 +0100

    Update

--- src/components/Content/Revue/RevuesMystere.jsx
+++ src/components/Content/Revue/RevuesMystere.jsx
@@ -4,22 +4,22 @@ export function RevuesMystere (props) {
             <div className="mystere-revues-container">
                 <div className="mystere-2-container">
                     <div className="mystere-2-title-container">
-                            <p className="mystere-2-title">Numéro 2 : La liberté en politique <a href="/mystere2info"> ? </a></p> 
+                            <p className="mystere-2-title">Numéro 2 : La liberté en politique </p> 
                     </div>
 
                     <div className="mystere-2-cover">
-                        <img src={props.mystere2} alt="" className="mystere-2-img"/>
+                    <a href="/mystere2info"> <img src={props.mystere2} alt="" className="mystere-2-img"/></a>
                     </div>
 
                 </div>
 
                 <div className="mystere-1-container">
                     <div className="mystere-1-title-container">
-                            <p className="mystere-1-title">Numéro 1 : Contreverse <a href="/mystere1info"> ? </a></p>
+                            <p className="mystere-1-title">Numéro 1 : Contreverse</p>
                     </div>
 
                     <div className="mystere-1-cover">
-                        <img src={props.mystere1} alt="" className="mystere-1-img"/>
+                        <a href="/mystere1info"> <img src={props.mystere1} alt="" className="mystere-1-img"/> </a>
                     </div>
 
                 </div>
